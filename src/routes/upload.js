4ab4476938501feaf1fa56ff2b7b42f884ead2d3
oliver <huillca12@gmail.com>
2025-11-21 12:03:35 -0500

    21-11-2025 12y03 pm

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -5,6 +5,7 @@ const upload = multer({ dest: 'uploads/' });
 
 function setRoutes(app) {
 	// Registrar solo el middleware de multer y el controlador
+    // key 'excelFile' debe coincidir con el nombre del campo en el formulario
 	app.post('/api/upload', upload.single('excelFile'), uploadExcel);
 }
 
